f9e5f574bdbf1039971bdfd1c003973a412ff0d7
Jason Bursey <Jason@Jasons-MacBook-Pro-3.local>
2021-04-29 21:03:46 -0400

    neural networks code started

---
+++ neural_network/activation_fns.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+# Sigmoid activation function
+def sigmoid(z):
+    soln = 1/(1+np.exp(-z))
+    return(soln)
+
+# Sigmoin derivative
+def deriv_sigmoin(z):
+    soln = sigmoid(z)*(1-sigmoid(z))
+    return(soln)
+
+# Tanh activation function
+def tanh(z):
+    soln = (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
+    return(soln)
+
+# tanh derivative
+def deriv_tanh(z):
+    soln = 1/((np.cosh(z))**2)
+    return(soln)
+
+# Relu activation function 
+def ReLU(z):
+    soln = max(0, z)
+    return(soln)
+
+# Relu derivative
+def deric_ReLU(z):
+    if z > 0:
+        return(1)
+    else:
+        return(0)
---
+++ neural_network/first.py
@@ -0,0 +1,35 @@
+import numpy as np
+from activation_fns import *
+
+# Simple data for learing even vs. off. 1=odd, 2=even
+
+X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+Y = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+
+
+
+def nn_first(X, Y, lr, acceptable_error):
+
+    #loop until error rate becomes acceptibly low
+
+    # Input layer
+
+
+    # Hidden layer
+
+
+    # Ouput layer
+
+    # Compute error (prediction-actual) (get real fn)
+
+    # Compute gradient wi for all weight from hidden layer to output layer then back propogation
+
+    # Compute gradient wi for all weights from input later to hidden layer then  Back propogation
+
+    # update neural network weights 
+
+    # Return neural network
+
+    return()
+
+    
