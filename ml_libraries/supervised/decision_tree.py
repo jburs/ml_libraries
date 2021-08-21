5e84eb1f0d901344c8bd1c2c0fcc5629b5e26f00
Jason Bursey <jason.bursey1@gmail.com>
2021-08-20 20:18:27 -0400

    decision tree progress

--- ml_libraries/supervised/decision_tree.py
+++ ml_libraries/supervised/decision_tree.py
@@ -11,7 +11,7 @@ df = df[['Fare', 'Age', 'Survived']].copy()
 df = df.dropna()
 
 def gini_impurity(data):
-    """calculate gini index for given dataset gini = 1 - sum(pi**2)"""
+    """calculate gini index for given dataset gini = 1 - sum(p(i)**2)"""
 
     prob = []
     n = len(data.index) # total data points
@@ -35,7 +35,6 @@ def get_split(data):
     best_value = 0.0
     columns = data.columns
     gini_base = gini_impurity(data)
-    print('\ngini base: ', gini_base)
     n_rows = len(data.index)                # total number of rows of data before split
 
     # Fininding which split yields the best gini gain
@@ -60,7 +59,8 @@ def get_split(data):
             w_gini = gini_left * w_left + gini_right * w_right
             
 
-            # Calculate gini gain (we want to minimize w_gini for the smallest impurity)
+            # Calculate gini gain (we want to minimize w_gini for the smallest impurity ideal case is split is perfect Left=c1, Right=c2)
+            # why not just find min w_gin instead of uding gini_gain and gini_base vaiables?
             gini_gain = gini_base - w_gini
 
             # check if this is the best split so far, store values, update max_gini
@@ -68,28 +68,90 @@ def get_split(data):
                 best_feature = columns[i]
                 best_value = x
                 max_gain = gini_gain
-                print(best_feature, best_value, gini_gain)
+               
 
-    return best_feature, best_value
+    return best_feature, best_value, df_left, df_right
+
+
+def predict(split_data):
+    """ makes prediction based on most common class """
+
+
+    # returns the most common class and accuracy (value_counts sorts descending from largest count)
+    class_count = split_data.iloc[:,-1].value_counts()
+    classes = sorted(split_data.iloc[:,-1].unique())
+    prediction = class_count.index[0]
+    accuracy = class_count[prediction]/sum(class_count[i] for i in classes)
+
+    return prediction, accuracy
 
 
 
-print(df.head(5))
+
+
 def decision_tree(data, max_depth=3, min_samples=25):
     
+    tree = dict()
+    tree_list = []
     depth = 0
-    n = len(data.index)
+    building = True
+    end_node = False
+    right_dfs = []
 
     # if there is GINI to be gained, we split further
-    if (depth < max_depth) and n >= min_samples:
+    
 
-        # Get the best split
-        best_feature, best_value = get_split(data)
-
-
-    return 
-
+    #initialize root node
+    best_feature, best_value, df_left, df_right = get_split(data) 
+    tree_list.append([best_feature, best_value, 'root'])                 # Store split
+    right_dfs.append(df_right)                      # Store df_right 
 
 
+    while building == True:
+        depth += 1
 
-decision_tree(df)
+        # Loop through left datasets
+        n_left = len(df_left.index)
+        if (depth < max_depth) and n_left >= min_samples and end_node == False:
+            # Get split on left dataset
+            best_feature, best_value, df_left, df_right = get_split(df_left)
+
+            tree_list.append([best_feature, best_value, 'left'])                 # Store split
+            right_dfs.append(df_right)                      # Store df_right 
+
+
+        #else get split on previous right dataset
+        else:
+            end_node = True # hit end node on left side
+
+            # Predict on df_left
+            print(df_left.head())
+            prediction, accuracy = predict(df_left)
+            
+
+            # Get split on right dataset
+            best_feature, best_value, df_left, df_right = get_split(df_right)
+
+            tree_list.append([best_feature, best_value, 'right'])                 # Store split
+            right_dfs.append(df_right)                      # Store df_right 
+            #depth+= 1                                       # increment node depth
+
+
+            building = False
+
+
+
+
+
+    return tree
+
+
+
+
+tree = decision_tree(df)
+print(tree)
+
+
+
+
+
